65c73097fd5537180a9953d9a8c81f016d4a1c8d
HarshSharma0801 <harsh08_sharma@yahoo.com>
2025-06-27 19:21:04 +0530

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/add": "http://localhost:5000",
+      "/read": "http://localhost:5000",
+      "/edit": "http://localhost:5000",
+      "/delete": "http://localhost:5000",
+    },
+  },
+});
